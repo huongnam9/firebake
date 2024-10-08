c0dd7a66bdbb3a2915872dfd79a3d3d77dab7cbb
nguyen huong nam <nam@shinlee.ai>
2024-10-08 11:24:01 +0700

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      copy({
+        targets: [
+          { src: 'public/_redirects', dest: 'dist' }, // Copy _redirects to dist
+          { src: 'public/_headers', dest: 'dist' },   // Copy _headers to dist (optional)
+        ],
+        verbose: true,
+    }),
+  ],
   base: './',  // Ensure this is correct
   build: {
     outDir: 'dist'
